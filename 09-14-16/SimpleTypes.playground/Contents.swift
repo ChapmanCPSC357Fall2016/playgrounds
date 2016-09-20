c4fac310ade07cf9ebe7dee19a8cebd6c1d9e457
ryanburns31892 <rburns@chapman.edu>
2016-09-19 17:14:26 -0700

    playground update

--- 09-14-16/SimpleTypes.playground/Contents.swift
+++ 09-14-16/SimpleTypes.playground/Contents.swift
@@ -16,7 +16,7 @@ else {
 }
 
 // Computed initializer way (define & call)
-let isOdd : Bool = {
+var isOdd : Bool = {
     
     if num % 2 == 1 {
         return true
@@ -52,7 +52,7 @@ class Dog {
     private var nameChanges = 0
     private var _name : String = ""
     private var _color : UIColor = UIColor.black
-    
+
     var name : String {
         
         get {
@@ -71,6 +71,7 @@ class Dog {
         }
     }
     
+    // Read-only computed variable
     var color : UIColor {
         
         // By default, just a get (read only)
@@ -92,6 +93,151 @@ d.name = "sonny"
 d.name = "rover"
 
 d.name = "corey"
+
+class Horse
+{
+    private var _color : UIColor = UIColor.black
+    
+    var color : UIColor {
+        
+        //Read-only property
+        return _color
+    }
+    
+    var age : Int = 0 {
+        
+        // Setter observers
+        
+        willSet {
+            print("About to set age from \(self.age) to \(newValue)")
+        }
+        
+        didSet {
+            print("Finally set age from \(oldValue) to \(self.age)")
+            
+            if age == 0
+            {
+                _color = UIColor.black
+            }
+            else
+            {
+                let newAlpha = 1.0 / Double(age)
+                _color = UIColor.black.withAlphaComponent(CGFloat(newAlpha))
+            }
+        }
+    }
+}
+
+let h = Horse()
+h.color
+//h.color = UIColor.red
+h.age = 2
+h.color
+
+h.age = 10
+h.color
+
+// String characters
+for c in str.characters
+{
+    print(c)
+}
+
+var r = 0...10
+
+type(of: r)
+
+for i in 0...9
+{
+    print(i)
+}
+
+for j in r
+{
+    print(j)
+}
+
+var number = 9
+
+let exists = (0...5).contains(number)
+
+var z : Int
+var y : Int
+
+(z, y) = (1, 2)
+
+z
+y
+
+var tup = (1,5,6,2)
+
+tup.0
+tup.3
+
+var california = (name : "California", code: "CA")
+
+california.1
+california.code
+
+
+// Named tuples
+func remove(target: Character, fromString : String) -> (numRemoved : Int, newString : String)
+{
+    
+    var removed = 0
+    var newPhrase = ""
+    
+    for c in fromString.characters
+    {
+        if c == target
+        {
+            removed += 1
+        }
+        else
+        {
+            newPhrase.append(c)
+        }
+    }
+    
+    return (removed, newPhrase)
+}
+
+let something = remove(target: "l", fromString: str)
+something.numRemoved
+something.newString
+
+let states = ["CA", "NE", "AZ"]
+
+for state in states
+{
+    print(state)
+}
+
+// Tuple enumerations
+for (index, element) in states.enumerated()
+{
+    print("\(index): \(element)")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
